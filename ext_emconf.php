7c443a8ccca614976f5ad76bdb3d8bd0cc944f24
Sebastian Klein <sebastian.klein@marketing-factory.de>
2024-08-03 17:24:48 +0200

    [FEATURE] Support Site Sets (#15)
    You can now include the extension's configuration
    using the Site Sets in TYPO3 v13.
    
    Both in TYPO3 v12 and v13, it is still possible to
    use Static Includes in a TypoScript record.

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.9-13.0.99',
+            'typo3' => '12.4.9-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
