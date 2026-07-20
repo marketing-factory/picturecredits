00e366035f326eb6b3a229d8b9d9a9311afb3731
Sebastian Klein <sebastian.klein@marketing-factory.de>
2026-07-20 17:08:27 +0200

    [TASK] Provide backend module icon based on TYPO3 version

--- Configuration/Backend/Modules.php
+++ Configuration/Backend/Modules.php
@@ -1,6 +1,14 @@
 <?php
 
 use Mfc\Picturecredits\Controllers\BackendController;
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
+
+$typo3Version = VersionNumberUtility::convertVersionNumberToInteger(
+    VersionNumberUtility::getCurrentTypo3Version()
+);
+$moduleIconIdentifier = $typo3Version < 14000000
+    ? 'module-picturecredits-legacy'
+    : 'module-picturecredits-theme';
 
 return [
     'picturecredits' => [
@@ -8,7 +16,7 @@ return [
         'position' => ['after' => 'tools_ExtensionmanagerExtensionmanager'],
         'access' => 'systemMaintainer',
         'workspaces' => 'live',
-        'iconIdentifier' => 'module-picturecredits',
+        'iconIdentifier' => $moduleIconIdentifier,
         'path' => '/module/picturecredits/import',
         'labels' => 'LLL:EXT:picturecredits/Resources/Private/Language/locallang_mod.xlf',
         'extensionName' => 'Picturecredits',
--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -3,12 +3,12 @@
 use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
 
 return [
-    'module-picturecredits-main' => [
-        'provider' => SvgIconProvider::class,
-        'source' => 'EXT:picturecredits/Resources/Public/Icons/backend-module-main.svg'
-    ],
-    'module-picturecredits' => [
+    'module-picturecredits-legacy' => [
         'provider' => SvgIconProvider::class,
         'source' => 'EXT:picturecredits/Resources/Public/Icons/backend-module.svg'
+    ],
+    'module-picturecredits-theme' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:picturecredits/Resources/Public/Icons/backend-module-theme.svg'
     ],
 ];
